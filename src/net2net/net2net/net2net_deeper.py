a90b6f8328c8440503707a3055af84259ff9c6ba
Tom <tom.ravaud@eleves.enpc.fr>
2023-12-08 15:49:09 +0100

    Fixed the Net2Deeper method implementation (the convolution kernel and stride of the new convolutional layer had incorrect shapes), restructured the code and added some doc

--- src/net2net/net2net/net2net_deeper.py
+++ src/net2net/net2net/net2net_deeper.py
@@ -1,144 +1,87 @@
 """
 An implementation of the Net2DeeperNet algorithm to deepen a network of convolutional layer
 """
-
-
+# Import libraries
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import numpy as np
 import copy
 
+# Import custom modules and packages
+from models.lenet import LeNet
 
-class LeNetWithoutBN(nn.Module):
+
+def add_modules(model: nn.Module,
+                target_module_name: str,
+                new_conv: nn.Module,
+                new_bn: nn.Module):
+    """Add new modules to a neural network. The new modules are added after a
+    a convolutional layer (target_module_name), followed by a batch
+    normalization layer and a ReLU activation. The new modules are composed of
+    a convolutional layer (new_conv), followed by a batch normalization layer
+    (new_bn) and a ReLU activation.
+
+    Args:
+        model (nn.Module): The neural network to which the new modules are
+        added
+        target_module_name (str): The name of the convolutional layer after
+        which the new modules are added (in reality, the modules are added
+        after the batch normalization layer and the ReLU activation following
+        the convolutional layer)
+        new_conv (nn.Module): The convolutional layer to add to the network
+        new_bn (nn.Module): The batch normalization layer to add to the network
     """
-    LeNet model
-    (Gradient-based learning applied to document recognition, LeCun et al.,
-    1998, http://vision.stanford.edu/cs598_spring07/papers/Lecun98.pdf)
-    where sigmoid activations are replaced by ReLU activations, and max pooling
-    is used instead of average pooling.
-    """
-    def __init__(self, nb_classes: int):
-        """Constructor of the class
-
-        Args:
-            input_size (int): Size of the input
-        """        
-        super(LeNetWithoutBN, self).__init__()
-        
-        # Define the components of the network
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=0),
-            nn.BatchNorm2d(6),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        
-        self.fc1 = nn.Linear(400, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, nb_classes)
-        self.relu = nn.ReLU()
     
-    
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass of the network
-
-        Args:
-            x (torch.Tensor): Input of the network
-
-        Returns:
-            torch.Tensor: Output of the network
-        """        
-        # Apply the network to the input
-        # Convolutions
-        out = self.layer1(x)
-        out = self.layer2(out)
-        
-        # Flatten the output
-        out = out.reshape(out.size(0), -1)
-        
-        # Fully connected layers
-        out = self.relu(self.fc1(out))
-        out = self.relu(self.fc2(out))
-        out = self.fc3(out)
-        
-        return out
-
-
-
-def add_module(model: nn.Module,
-                   target_module_name: str):
-    # Get the original convolutional layer from the model
-    #original_layer = getattr(model, target_module_name)
-
-    # Split the original convolutional layer
     # Divide the module name into its components
     module_path = target_module_name.split('.')
-    print(module_path)
-    current_module = getattr(model, module_path[0])
-
     
-    # If the module is at the top level
+    # Create a list of the modules and their names starting from the
+    # previous convolutional layer
     if len(module_path) == 1:
-        listaftermodulename  = []
+        
+        # Create a list to store the name and the module of the modules after
+        # the previous convolutional layer
+        list_after_module_name = []
+        
+        # Go through the modules of the network
         for name, module in model.named_modules():
-            print(name)
-            print(module)
-            if name == module_path[0] or len(listaftermodulename)!=0:
-                listaftermodulename.append([name,module])
-        
-        new_module = nn.Conv2d(in_channels=current_module.in_channels,
-                            out_channels=current_module.out_channels,
-                            kernel_size=current_module.kernel_size,
-                            stride=current_module.stride,
-                            padding=current_module.padding)
-        
-        weight = new_module.weight
-        deeper_w = np.zeros((weight.shape[0], weight.shape[1], weight.shape[3], weight.shape[3]))
-        #assert weight.shape[0] % 2 == 1 and weight.shape[1]% 2 ==1, 'Kernel size should be odd'
-        center_h = (weight.shape[0]-1)//2
-        center_w = (weight.shape[1]-1)//2
-        for i in range(weight.shape[3]):
-            tmp = np.zeros((weight.shape[0], weight.shape[1], weight.shape[3]))
-            tmp[center_h, center_w, i] = 1
-            deeper_w[:, :, :, i] = 1
-        deeper_b = np.zeros(weight.shape[3])
-
-        new_module.modified_state_dict = {}
-
-        new_module.modified_state_dict["new_module" + ".weight"] = deeper_w
-        new_module.modified_state_dict["new_module" + ".bias"] = deeper_b
-        new_module.load_state_dict(new_module.modified_state_dict,strict=False)
-        
-        
-        setattr(model, str(int(target_module_name) + 3), new_module)
-        setattr(model, str(int(target_module_name) + 4), nn.BatchNorm2d(current_module.out_channels))
+            
+            # Check if the current module is after the previous convolutional
+            # layer
+            if name == module_path[0] or len(list_after_module_name)!=0:
+                
+                list_after_module_name.append((name, module))
+                
+        # Insert the new modules (ie {convolutional layer + batch normalization
+        # layer + ReLU activation}) to the network after the previous
+        # convolutional layer (we assume that this layer is followed by a batch
+        # normalization layer and a ReLU activation, it is why we add 3, 4 and
+        # 5 to the module name)
+        setattr(model, str(int(target_module_name) + 3), new_conv)
+        setattr(model, str(int(target_module_name) + 4), new_bn)
         setattr(model, str(int(target_module_name) + 5), nn.ReLU())
         
-        if len(listaftermodulename)>3:
-            for i in range(1,len(listaftermodulename)-2):
-                setattr(model, str(int(listaftermodulename[-i][0]) + 3), listaftermodulename[-i][1])
-
-
-
-
-
-
-
-        # Remove the original layer from the model
-        #delattr(model, target_module_name)
-
+        # If there were other modules after the three new modules we added,
+        # add them back to the network with an updated name
+        if len(list_after_module_name) > 3:
+            
+            # Go through the modules after the three new modules we added
+            for i in range(1, len(list_after_module_name) - 2):
+                
+                # Add the module back to the network with an updated name
+                setattr(model,
+                        str(int(list_after_module_name[-i][0])+3),
+                        list_after_module_name[-i][1])
+        
     else:
         # Get the current module
         current_module_name = module_path[0]
         current_module = getattr(model, current_module_name)
         
-        # Recursively replace the module
-        add_module(current_module, '.'.join(module_path[1:]))
+        # Recursively call the function to add the new modules to the network
+        add_modules(current_module,
+                    '.'.join(module_path[1:]),
+                    new_conv, new_bn)
 
 
 class Net2Net:
@@ -154,66 +97,118 @@ class Net2Net:
 
     
     def net2deeper(self,
-                  target_layer: str):
+                   deeper_operations: dict):
         """Deepen a layer of a neural network
 
         Args:
-            target_layer (str): Layer to be deepen
+            deeper_operations (dict): A dictionary containing the operations
+            to perform to deepen the network.
+        """  
+
+        return
+
+
+    def net2deeper_operation(self, previous_conv: str):
+        """Deepen a layer of a neural network by adding a convolutional layer
+        followed by a batch normalization layer and a ReLU activation.
+
+        Args:
+            previous_conv (str): The name of the convolutional layer after
+            which the new modules are added (in reality, the modules are added
+            after the batch normalization layer and the ReLU activation
+            following the convolutional layer)
+
+        Raises:
+            NotImplementedError: If the stride of the convolutional layer is
+            different from 1
         """
-
-
-        with torch.no_grad():
-            # Create a copy of the student network
-            new_student_network = copy.deepcopy(self.student_network)
-
-            for name, module in self.student_network.named_modules():
-
-                if name == target_layer :
-                    # Assuming you want to double the number of output channels
-                    """
-                    new_module = nn.Conv2d(
-                        in_channels=module.in_channels,
-                        out_channels=module.out_channels,
-                        kernel_size=module.kernel_size,
-                        stride=module.stride,
-                        padding=module.padding)"""
-                    add_module(new_student_network,
-                                   name)
-                    print(new_student_network)
-
-            # Get the parameters of the target layer
-
-            self.student_network = new_student_network
-
+        
+        # Go through the modules of the network
+        for name, module in self.student_network.named_modules():
             
+            # Check if the current module is the one to be copied
+            if name == previous_conv:
+                
+                # Create a new module, ie copy the current convolutional layer
+                # (the number of input channels must match the number of output
+                # channels of the previous convolutional layer !)
+                # (with some padding to keep the size of the feature maps 
+                # unchanged, assuming the stride is 1)
+                if module.stride == (1, 1):
+                    new_conv = nn.Conv2d(module.out_channels,
+                                         module.out_channels,
+                                         kernel_size=module.kernel_size,
+                                         stride=1,
+                                         padding=((module.kernel_size[0]-1)//2,
+                                                  (module.kernel_size[1]-1)//2))
+                else:
+                    raise NotImplementedError(
+                        "Convolutional layers with stride different from 1"
+                        "are not implemented yet.")
+                
+                # Create a new batch normalization layer
+                new_bn = nn.BatchNorm2d(module.out_channels)
+                
+                break
+            
+        # Initialize the weights and bias of the new convolutional layer to be
+        # an identity function and a zero vector, respectively
+        new_weights = np.zeros_like(new_conv.weight.data)
+        
+        # Put a 1 in the center of each convolutional kernel
+        for i in range(new_weights.shape[0]):
+            for j in range(new_weights.shape[1]):
+                new_weights[i,
+                            j,
+                            new_weights.shape[2]//2,
+                            new_weights.shape[3]//2] = 1
+        
+        # Initialize the bias to zero
+        new_bias = np.zeros_like(new_conv.bias.data)
+        
+        # Load the weights and bias of the new convolutional layer
+        new_conv.weight.data = torch.from_numpy(new_weights)
+        new_conv.bias.data = torch.from_numpy(new_bias)
+        
+        # TODO: Initialize the weights and bias of the new batch normalization
+        print("The weights and bias of the new batch normalization layer are"
+              "not initialized yet. To be implemented.")
+        
+        # Add the new modules (ie {convolutional layer + batch normalization
+        # layer + ReLU activation}) to the network after the previous
+        # convolutional layer
+        add_modules(self.student_network, previous_conv, new_conv, new_bn)
+        
 
 if __name__ == '__main__':
 
     # TEST 
     # Create a model
-    model2 = LeNetWithoutBN(nb_classes=10)
+    model = LeNet(nb_classes=10)
     
     # Instantiate a Net2Net object from a (pre-trained) model
-    net2net = Net2Net(teacher_network=model2)
+    net2net = Net2Net(teacher_network=model)
     
-    # Set a layer to be deepen
-    target_layer = "layer1.0"
+    
+    # Set the convolutional layer to copy in order to deepen the network
+    # (we assume that this layer is followed by a batch normalization layer
+    # and a ReLU activation)
+    previous_conv = "layer1.0"
     
     # Depen a layer of the network
-    net2net.net2deeper(target_layer=target_layer)
+    net2net.net2deeper_operation(previous_conv=previous_conv)
     
     # Create a random input
     x = torch.randn(1, 1, 32, 32)
     
     # Compute the output of the teacher network
-    y_teacher = model2(x)
+    y_teacher = model(x)
     
     # Compute the output of the student network
     y_student = net2net.student_network(x)
-    #print(model2)
-    #print(net2net.student_network)
     
     # The outputs should be the same
-    print("TEST2 (LeNetWithoutBN): The outputs of the teacher and student networks should be the same.")
+    print("TEST (LeNet): The outputs of the teacher and student networks"
+          "should be the same.")
     print("Teacher output: ", y_teacher)
     print("Student output: ", y_student, "\n")
